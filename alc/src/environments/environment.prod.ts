7ae9e16593d30846f40cd86180970784d205bf54
Vivipe28 <viviana.perez28@gmail.com>
2022-07-30 17:51:16 -0500

    environment changes

--- alc/src/environments/environment.prod.ts
+++ alc/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlApi: "http://challenge-react.alkemy.org/",
-  ApiMenu: "https://api.spoonacular.com",
+  urlApi: "//challenge-react.alkemy.org/",
+  ApiMenu: "//api.spoonacular.com",
 };
