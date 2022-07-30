8236bff53dcd647e69fac0b3916c61a34a836d60
Vivipe28 <viviana.perez28@gmail.com>
2022-07-30 18:29:07 -0500

    replace https to http

--- alc/src/environments/environment.prod.ts
+++ alc/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlApi: "//challenge-react.alkemy.org/",
-  ApiMenu: "//api.spoonacular.com",
+  urlApi: "http://challenge-react.alkemy.org/",
+  ApiMenu: "http://api.spoonacular.com",
 };
